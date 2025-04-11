36bce86337994affcb338dfa48fcabf8e74012f3
Jodo1010 <106851141+Jodo1010@users.noreply.github.com>
2025-04-10 22:00:04 -0400

    Update statements.js

--- js/statements.js
+++ js/statements.js
@@ -3,7 +3,7 @@
 
 const statements = [
     {
-        institution: "Eastern Michigan University Library (Halle Library)",
+        institution: "Halle Library",
         category: "university",
         dateArchived: "April 10, 2025",
         preview: "The faculty and staff of the Eastern Michigan University Library value diversity, equity, and inclusion in our work and in our communities. We are committed to creating an environment that values and supports the differences in our communities...",
